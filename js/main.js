962af9721be1eada9c8a4ded9cbf7fffb61d429d
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-09-01 14:39:54 +0600

    video thumbnail is loading dynamically

--- js/main.js
+++ js/main.js
@@ -3,15 +3,34 @@ const fetchData = async () =>{
     const data = await res.json();
     const categoriesArray = await data.data;
    for(let categories of categoriesArray){
-    const category = categories.category;
-   displayCategory(category);
+   displayCategory(categories);
    }
 };
 fetchData();
-const displayCategory = (category) =>{
+const displayCategory = (categories) =>{
     const categoryNameContainer = document.getElementById("category-name-container");
     const button = document.createElement("button");
-    button.classList.add("btn")
-    button.innerText = `${category}`;
+    button.innerHTML = `<button onclick="loadDataBasedOnButtonClick('${categories.category_id}')" class="btn">${categories.category}</button>`;
     categoryNameContainer.append(button);
+};
+const loadDataBasedOnButtonClick = async (id) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const data = await res.json();
+    const videos = await data.data;
+    displayVideos(videos);
+};
+loadDataBasedOnButtonClick();
+const displayVideos = (videos) =>{
+    const videosContainer = document.getElementById("videos-container");
+   for (let video of videos){
+    console.log(video);
+    const videoDiv = document.createElement("div");
+    videoDiv.innerHTML = `
+        <div class="w-[312px] h-[200px] rounded-lg"><img src="${video.thumbnail}" class="w-full h-full">
+        
+        </div>
+    `;
+    videosContainer.append(videoDiv);
+   }
 }
+
